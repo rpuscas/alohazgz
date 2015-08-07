d0d6d1268810da63f9ec906bc126b46902b82d0c
Razvan <rpuscas94@gmail.com>
2015-08-07 17:21:25 +0200

    use geocoder gem to geolocalize places without coordinates

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'httparty'
 
+gem 'geocoder'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,18 @@
 class Place < ActiveRecord::Base
-	
+
+  before_save :geolocalize
+
+  private
+
+	  def geolocalize
+	    if self.latitude==0 and self.longitude==0
+	      results = Geocoder.search(address)
+
+	      if results.size > 0
+	        self.latitude = results.first.latitude
+	        self.longitude = results.first.longitude
+	      end
+	    end
+	  end	
 
 end
